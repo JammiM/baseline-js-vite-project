5347c97c65e11d9b0d430addef3f4cb52c27a184
James Morrissey <jammim19@gmail.com>
2026-04-24 13:58:10 +0100

    Added a simple test

---
+++ src/js/sum.test.js
@@ -0,0 +1,11 @@
+import { sum } from "./sum.js";
+
+describe("Initial test", () => {
+  it("works", () => {
+    expect(true);
+  });
+
+  it("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
